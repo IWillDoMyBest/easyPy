8bae0de81a9f3094c15aba991fc8c1d8bea5d566
elias0012 <elias.wt@hotmail.com>
2018-02-09 00:32:10 +0100

    v1.5

--- app/math.py
+++ app/math.py
@@ -1,7 +1,7 @@
 import sys, os
-from ..ext.errorHandling import listError, valueError
+from errorHandling import listError, valueError
 
-def pq(x, p, q): 
+def pq(x, p, q): #Pq-formeln
 	try:
 		if isinstance(x, int or float) and isinstance(p, int or float) and (q, int or float):
 			if x != 1:
@@ -14,7 +14,7 @@ def pq(x, p, q):
 	except Exception as error:
 		print(error, file=sys.stderr), sys.exit()
 
-def root(x):
+def root(x): #Roten ur
 	try:
 		if not isinstance(x, int):
 			print(valueError, file=sys.stderr), sys.exit()
@@ -25,11 +25,36 @@ def root(x):
 	except Exception as error:
 		print(error, file=sys.stderr), sys.exit()
 
-def distance(x, y):
+def distance(x, y): #
 	try:
 		if not isinstance(x, (int, float)) and not isinstance(y, (int, float)):
 			print(valueError, file=sys.stderr), sys.exit()
 		return round(((x**2) +(y)**2)**0.5, 2) 
 	except Exception as error:
 		print(error, file=sys.stderr), sys.exit()
+
+def sqrRulepos(x, y): #Första kvadreringsregeln
+	try:
+		if not isinstance(x, (int, float)) and not isinstance(y, (int, float)):
+			print(valueError, file=sys.stderr), sys.exit()
+		return round((x+y)**2)
+	except Exception as error:
+		print(error, file=sys.stderr), sys.exit()
+
+def sqrRuleneg(x,y): #Andra kvadreringsregeln
+	try:
+		if not isinstance(x, (int, float)) and not isinstance(y, (int, float)):
+			print(valueError, file=sys.stderr), sys.exit()
+		return round((x-y)**2)
+	except Exception as error:
+		print(error, file=sys.stderr), sys.exit()
+
+def slope(x1,x2,y1,y2): #Lutningen på en rät linje
+	try: 
+		if not isinstance(x1, (int, float)) and not isinstance(x2, (int, float)) and not (y1, (int, float)) and not (y2, (int, float)):
+			print(valueError, file=sys.stderr), sys.exit()
+		return round((y2-y1)/(x2-x1))
+	except Exception as error:
+		print(error, file=sys.stderr), sys.exit()
+
 
